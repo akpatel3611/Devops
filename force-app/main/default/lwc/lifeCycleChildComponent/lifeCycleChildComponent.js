99697595fdd36ea8a4ce679f83d02e72f1b8c376
ankit patel <akpatel3611@gmail.com>
2026-07-10 11:26:11 +0530

    Commit changes for story a0fWU0000043auHYAQ

--- force-app/main/default/lwc/lifeCycleChildComponent/lifeCycleChildComponent.js
+++ force-app/main/default/lwc/lifeCycleChildComponent/lifeCycleChildComponent.js
@@ -1,18 +1,1 @@
-import { LightningElement } from 'lwc';
-
-export default class LifeCycleChildComponent extends LightningElement {
-
-    constructor(){
-        super();
-        console.log('Child Constructor Called');
-    }
-
-    connectedCallback(){
-        console.log('Child connectedCallback Called');
-    }
-     
-    renderedCallback(){
-        console.log('Child renderedCallback Called');
-    }
-    
-}
+/* Simulated metadata content for lifeCycleChildComponent (LightningComponentBundle) */
