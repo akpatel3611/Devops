27ace541a261dc79fb38393c26d3b8c1b3084de3
ankit patel <akpatel3611@gmail.com>
2026-07-21 14:56:45 +0530

    test commit

--- force-app/main/default/lwc/test/test.js
+++ force-app/main/default/lwc/test/test.js
@@ -3,6 +3,9 @@ import { api, LightningElement } from 'lwc';
 export default class Test extends LightningElement {
     @api inputValue = '';
 
+    connectedCallback() {
+        // No initialization needed for inputValue in connectedCallback
+    }
     handleInputChange(event) {
         this.inputValue = event.target.value;
     }
