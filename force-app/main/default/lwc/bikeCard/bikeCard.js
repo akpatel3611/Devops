691e28c25d8efd6989d651319cf13367ad7c704b
ankit patel <akpatel3611@gmail.com>
2026-07-30 17:34:09 +0530

    test form us 34

--- force-app/main/default/lwc/bikeCard/bikeCard.js
+++ force-app/main/default/lwc/bikeCard/bikeCard.js
@@ -1,6 +1,21 @@
 import { LightningElement } from 'lwc';
 
 export default class BikeCard extends LightningElement {
+    constructor() {
+        super();
+        console.log('BikeCard component initialized');
+    }
+    connectedCallback() {
+        console.log('connectedCallback called');
+    }
+    renderedCallback() {
+        console.log('renderedCallback called');
+    }
+     disconnectedCallback() {
+        console.log('disconnectedCallback called');
+    }
+    // No additional methods or logic needed here
+    //console.log('BikeCard component disconnected');
     bike = {
         name: 'Trail Blazer',
         type: 'Mountain',
